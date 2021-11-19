1541dc15b4c02ee96792268b4ff139e8c83c3b21
KarlSherman1999 <karlsherman1999@gmail.com>
2021-11-19 15:07:38 +0000

    keycodesite#

---
+++ scripts.js
@@ -0,0 +1,136 @@
+let key = document.getElementById("key");
+let which = document.getElementById("which");
+let codekey = document.getElementById("code");
+
+document.addEventListener("keydown",(event)=>{
+    if (event.code == "KeyA"){
+        key.innerHTML="a"
+        which.innerHTML="65"
+        codekey.innerHTML="KeyA"
+    }
+    else if (event.code == "KeyB"){
+        key.innerHTML="b"
+        which.innerHTML="66"
+        codekey.innerHTML="KeyB"
+    }
+    else if (event.code == "KeyC"){
+        key.innerHTML="c"
+        which.innerHTML="67"
+        codekey.innerHTML="KeyC"
+    }
+    else if (event.code == "KeyD"){
+        key.innerHTML="d"
+        which.innerHTML="68"
+        codekey.innerHTML="KeyD"
+    }
+    else if (event.code == "KeyE"){
+        key.innerHTML="e"
+        which.innerHTML="69"
+        codekey.innerHTML="KeyE"
+    }
+    else if (event.code == "KeyF"){
+        key.innerHTML="f"
+        which.innerHTML="70"
+        codekey.innerHTML="KeyF"
+    }
+    else if (event.code == "KeyG"){
+        key.innerHTML="g"
+        which.innerHTML="71"
+        codekey.innerHTML="KeyG"
+    }
+    else if (event.code == "KeyH"){
+        key.innerHTML="h"
+        which.innerHTML="72"
+        codekey.innerHTML="KeyH"
+    }
+    else if (event.code == "KeyI"){
+        key.innerHTML="i"
+        which.innerHTML="73"
+        codekey.innerHTML="KeyI"
+    }
+    else if (event.code == "KeyJ"){
+        key.innerHTML="j"
+        which.innerHTML="74"
+        codekey.innerHTML="KeyJ"
+    }
+    else if (event.code == "KeyK"){
+        key.innerHTML="k"
+        which.innerHTML="75"
+        codekey.innerHTML="KeyK"
+    }
+    else if (event.code == "KeyL"){
+        key.innerHTML="l"
+        which.innerHTML="76"
+        codekey.innerHTML="KeyL"
+    }
+    else if (event.code == "KeyM"){
+        key.innerHTML="m"
+        which.innerHTML="77"
+        codekey.innerHTML="KeyM"
+    }
+    else if (event.code == "KeyN"){
+        key.innerHTML="n"
+        which.innerHTML="78"
+        codekey.innerHTML="KeyN"
+    }
+    else if (event.code == "KeyO"){
+        key.innerHTML="o"
+        which.innerHTML="79"
+        codekey.innerHTML="KeyO"
+    }
+    else if (event.code == "KeyP"){
+        key.innerHTML="p"
+        which.innerHTML="80"
+        codekey.innerHTML="KeyP"
+    }
+    else if (event.code == "KeyQ"){
+        key.innerHTML="q"
+        which.innerHTML="81"
+        codekey.innerHTML="KeyQ"
+    }
+    else if (event.code == "KeyR"){
+        key.innerHTML="r"
+        which.innerHTML="82"
+        codekey.innerHTML="KeyR"
+    }
+    else if (event.code == "KeyS"){
+        key.innerHTML="s"
+        which.innerHTML="83"
+        codekey.innerHTML="KeyS"
+    }
+    else if (event.code == "KeyT"){
+        key.innerHTML="t"
+        which.innerHTML="84"
+        codekey.innerHTML="KeyT"
+    }
+    else if (event.code == "KeyU"){
+        key.innerHTML="u"
+        which.innerHTML="85"
+        codekey.innerHTML="KeyU"
+    }
+    else if (event.code == "KeyV"){
+        key.innerHTML="v"
+        which.innerHTML="86"
+        codekey.innerHTML="KeyV"
+    }
+    else if (event.code == "KeyW"){
+        key.innerHTML="w"
+        which.innerHTML="87"
+        codekey.innerHTML="KeyW"
+    }
+    else if (event.code == "KeyX"){
+        key.innerHTML="x"
+        which.innerHTML="88"
+        codekey.innerHTML="KeyX"
+    }
+    else if (event.code == "KeyY"){
+        key.innerHTML="y"
+        which.innerHTML="89"
+        codekey.innerHTML="KeyY"
+    }
+    else if (event.code == "KeyZ"){
+        key.innerHTML="z"
+        which.innerHTML="90"
+        codekey.innerHTML="KeyZ"
+    }
+})
